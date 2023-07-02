fd62089b611379183fd03bc4a2f548d36709ad7f
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-07-03 00:07:57 +0700

    update song

---
+++ src/main/java/com/programming/karaoke/model/Song.java
@@ -0,0 +1,18 @@
+package com.programming.karaoke.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(value = "song")
+public class Song {
+    @Id
+    private String songId;
+    private String songImg;
+    private String songLyric;
+    private Long songDuration;
+    private String artist;
+    private String songBeat;
+    private String songFormat;
+}
