31ca3ff17083a8f46da4db5cbc77ce7ec0f64272
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-07-04 00:32:49 +0700

    song update

--- src/main/java/com/programming/karaoke/controller/SongController.java
+++ src/main/java/com/programming/karaoke/controller/SongController.java
@@ -1,16 +1,15 @@
 package com.programming.karaoke.controller;
 
+import com.programming.karaoke.model.Song;
 import com.programming.karaoke.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/songs")
@@ -18,15 +17,17 @@ public class SongController {
     @Autowired
     private SongService songService;
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadSong(@RequestParam("songName") String songName,
-                                             @RequestParam("soundFile") MultipartFile soundFile) {
-        try {
-            songService.createSongFolder(songName);
-            songService.saveFiles(songName, soundFile);
-            return ResponseEntity.status(HttpStatus.OK).body("Song uploaded successfully.");
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload song.");
-        }
+    @PostMapping("/create")
+    public ResponseEntity<String> createSong(@RequestParam("songFile") MultipartFile songFile,
+                                             @RequestParam(value = "imgFile", required = false) MultipartFile imgFile,
+                                             @RequestParam(value = "lyricFile", required = false) MultipartFile lyricFile) {
+        songService.createSong(songFile, imgFile, lyricFile);
+        return ResponseEntity.ok("Song created successfully.");
+    }
+
+    @GetMapping("/getAllSongs")
+    public ResponseEntity<List<Song>> getAllSongs() {
+        List<Song> songs = songService.getAllSongs();
+        return ResponseEntity.ok(songs);
     }
 }
