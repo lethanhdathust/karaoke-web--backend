3e067418707f246f998e93e0a80ceb306ac8044e
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-07-04 08:32:42 +0700

    pull

--- src/main/java/com/programming/karaoke/controller/SongController.java
+++ src/main/java/com/programming/karaoke/controller/SongController.java
@@ -4,6 +4,7 @@ import com.programming.karaoke.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,13 +20,13 @@ public class SongController {
     private SongService songService;
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadSong(@RequestParam("songName") String songName,
-                                             @RequestParam("soundFile") MultipartFile soundFile) {
+    public ResponseEntity<String> uploadSong(@RequestParam("soundFile") MultipartFile soundFile) {
         try {
+            String songName = StringUtils.stripFilenameExtension(soundFile.getOriginalFilename());
             songService.createSongFolder(songName);
             songService.saveFiles(songName, soundFile);
-            return ResponseEntity.status(HttpStatus.OK).body("Song uploaded successfully.");
-        } catch (IOException e) {
+            return ResponseEntity.ok("Song uploaded successfully.");
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload song.");
         }
     }
